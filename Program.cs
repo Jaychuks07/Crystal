51e37bf30080c16560a71063cf1a9e4c111fff2e
Joseph <joseph.datapex@gmail.com>
2019-01-08 23:00:33 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Crystal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter num1! ");
+
+            int num1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter num2! ");
+
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            int result = num1 + num2;
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
